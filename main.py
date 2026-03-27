a82f8a4742dff6519b405b0fac12872edc0b075d
Dustin Jones <dustin.jones2@g.austincc.edu>
2026-03-26 21:24:01 -0500

    implemented player triangle sprite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,14 +8,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
