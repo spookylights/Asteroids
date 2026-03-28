407f6bcced286a54b1b8e243ba7017fd33006bd8
Dustin Jones <dustin.jones2@g.austincc.edu>
2026-03-27 20:52:04 -0500

    implement vertical movement and groups

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
